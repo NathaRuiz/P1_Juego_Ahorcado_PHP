2608f5cee190d53970e7027784ca258f1eb60448
NathaRuiz <nruizm98@gmail.com>
2024-01-21 13:46:52 +0100

    modified images sizes and adjust tasks html

--- src/php/game.php
+++ src/php/game.php
@@ -3,30 +3,27 @@ session_start();
 startGame();
 
 $won = true;
-$bodyParts = ["rightLeg","leftLeg" ,"arms" ,"body" ,"head" ,"noHead"];
+$bodyParts = ["rightLeg", "leftLeg", "arms", "body", "head", "noHead"];
 
 if (isset($_POST['letter'])) {
     wordHasTheLetter();
 }
 
-//Restart
 if (isset($_GET['Restart'])) {
-    echo '<script>console.log("entró")</script>';
-   restart();
+    restart();
 }
-// Check if the game is won or lost
-
 
 checkRulesGame();
 
-function restart(){
+function restart()
+{
     session_unset();
     session_destroy();
     header("location: index.php");
 }
 
-// Initialize the game if not already started
-function startGame(){
+function startGame()
+{
     if (!isset($_SESSION['word'])) {
         $words = array(
             "blanco",
@@ -55,61 +52,70 @@ function startGame(){
         $_SESSION['wrong_letters'] = [];
         $_SESSION['attempts'] = 5;
     } else {
-        // Ensure guessed_letters is an array
         if (!isset($_SESSION['guessed_letters']) || !is_array($_SESSION['guessed_letters'])) {
             $_SESSION['guessed_letters'] = [];
         }
     }
 }
 
-// Check if a letter is submitted
-function wordHasTheLetter(){
+function wordHasTheLetter()
+{
     $guessedLetter = strtolower($_POST['letter']);
 
-    // Check if the guessed letter is valid
     if (ctype_alpha($guessedLetter) && strlen($guessedLetter) === 1) {
-        // Check if the letter is in the word
         if (strpos($_SESSION['word'], $guessedLetter) === false) {
-            $_SESSION['attempts']--;
-            $_SESSION['wrong_letters'][] = $guessedLetter;
+            if (!in_array($guessedLetter, $_SESSION['wrong_letters'])) {
+                $_SESSION['attempts']--;
+                $_SESSION['wrong_letters'][] = $guessedLetter;
+            }
         } else {
             $_SESSION['guessed_letters'][] = $guessedLetter;
-        }       
+        }
     }
 }
 
-function checkRulesGame(){
+function checkRulesGame()
+{
     global $won;
     isSameWord();
-    if($_SESSION['attempts'] !== 0 && !$won){
-        echo "Attempts remaining: {$_SESSION['attempts']}<br>";
-        echo "Wrong letters: " . implode(", ", $_SESSION['wrong_letters']) . "<br>";
-        echo "<img  class='image' src=". getCurrentImage()." >";
-        // Display the word with underscores for unguessed letters
+    if ($_SESSION['attempts'] !== 0 && !$won) {
+        echo '<h2 class="title">El Ahorcado de los Colores</h2>';
+        echo '<div class="infoGame">';
+        echo "<span>Número de intentos: {$_SESSION['attempts']} </span>";
+        echo "<span>Letras incorrectas: " . implode(", ", $_SESSION['wrong_letters']) . "</span>";
+        echo '</div>';
+        echo '<div class="guessedLetterContainer" > 
+               <img  class="image" src=" '. getCurrentImage() . '" >
+               <div class="guessedLetter">
+               <div class="secretWord">';
+
         foreach (str_split($_SESSION['word']) as $letter) {
             if (in_array($letter, $_SESSION['guessed_letters'])) {
+                echo "<span>";
                 echo $letter . " ";
+                echo "</span>";
             } else {
+                echo "<span>";
                 echo "_ ";
+                echo "</span>";
             }
         }
-        // Display the form for guessing a letter
+        echo '</div>';
         echo '
-            <form method="post" action="" class="formReload">
-                <label for="letter">Guess a letter:</label>
-                <input type="text" name="letter" maxlength="1" pattern="[a-zA-Z]" required>
-                <input type="submit" value="Guess">
-            </form> 
-            <form method="get" action="" class="formAddLetter">
-                <input type="submit" name="Restart" value="Restart">
-            </form> ';
-    }
-    else{
+            <form method="post" class="formAddLetter">
+            <label for="letter">Ingresa una letra:</label>
+            <input type="text" name="letter" maxlength="1" pattern="[a-zA-Z]" required>
+            <input type="submit" value="Enviar">
+        </form> ';
+        echo '</div>
+            </div>';
+    } else {
         resultGame();
     }
 }
 
-function isSameWord(){
+function isSameWord()
+{
     global $won;
     foreach (str_split($_SESSION['word']) as $letter) {
         if (!in_array($letter, $_SESSION['guessed_letters'])) {
@@ -119,31 +125,30 @@ function isSameWord(){
     }
 }
 
-function resultGame(){
+function resultGame()
+{
     global $won;
     if ($_SESSION['attempts'] === 0) {
-        echo "You lost! The correct word was: {$_SESSION['word']}";
+        echo "<h1>¡Has perdido!</h1>;";
+        echo " <div><span>La palabra correcta era: {$_SESSION['word']} </span>";
         echo '<form method="get" action="">
         <input type="submit" name="Restart" value="Restart">
-    </form> ';
-    echo "<img class='image' src=". getCurrentImage()." >";
-       
+    </form> </div>';
+        echo "<img class='image' src=" . getCurrentImage() . " >";
     }
     if ($won) {
-        echo "Congratulations! You guessed the word: {$_SESSION['word']}";
-        echo '<form method="get" action="">
+        echo "<h1>¡Felicidades!</h1>;";
+        echo "adivinaste la palabra : {$_SESSION['word']}";
+        echo '<div><form method="get" action="">
         <input type="submit" name="Restart" value="Restart">
-    </form> ';
-        
-    } 
+    </form> </div>';
+    }
 }
 
-function getCurrentImage(){
+function getCurrentImage()
+{
     global $bodyParts;
     $part = $_SESSION['attempts'];
     $imgPart = $bodyParts[$part];
-    return "../assets/images/".$imgPart.".png";
+    return "../assets/images/" . $imgPart . ".png";
 }
-
-
-?>
--- src/php/index.php
+++ src/php/index.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,31 +10,22 @@
     <link href="https://fonts.googleapis.com/css2?family=Love+Ya+Like+A+Sister&display=swap" rel="stylesheet">
     <title>Juego del Ahorcado</title>
 </head>
+
 <body>
     <header>
         <h1>Minijuego</h1>
     </header>
     <main>
         <div class="blackboard ">
-   
+
             <div class=" blackboard-content">
-                <!-- <div class="formReload">
-                    <input type="submit" name="reload" value="Reiniciar juego" class="reload">
-                </div> -->
-                <!-- <h2 class="title">El Ahorcado de los Colores</h2> -->
-                <!-- <div class="formAddLetter">
-                    <label for="letter">Ingresa una letra:</label>
-                    <input type="text" id="letter" name="letter" maxlength="1" pattern="[a-zA-Z]" required>
-                    <input type="submit" name="submit" value="Enviar" class="submit">
-                </div> --><?php 
-                    include 'game.php';
-                    ?>
-                <div class="colorContainer">
-                    
-                </div>
+                <?php
+                include 'game.php';
+                ?>
             </div>
         </div>
     </main>
-   
+
 </body>
+
 </html>
